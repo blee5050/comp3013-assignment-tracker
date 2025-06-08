5437817cb7b19bd99c39fb25a0c213337beb3a91
blee5050 <163495634+blee5050@users.noreply.github.com>
2025-06-07 18:31:30 -0700

    test commit

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,6 +7,7 @@ export function Header() {
     <header className={styles.header}>
       {/* This is simply to show you how to use helper functions */}
       <h1>{uppercase("bcit")} Assignment Tracker</h1>
+      <h1>TEST</h1>
       <form className={styles.newAssignmentForm}>
         <input placeholder="Add a new assignment" type="text" />
         <button>
